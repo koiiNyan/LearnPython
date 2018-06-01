91e6889242ed63afedadebe8519ac6cd95a010cb
Anastasiya Ovinnikova <anovinnikova@yandex.ru>
2018-06-01 23:31:16 +0300

    hw

---
+++ lesson4/excel/xl.py
@@ -0,0 +1,33 @@
+from openpyxl import load_workbook
+
+def read_excel(filename):
+    workbook = load_workbook(filename)
+    worksheet = workbook['Sheet0']
+
+    
+    excel_data = list()
+
+    for row in range(2,40):
+        excel_row = dict()
+        if worksheet.cell(row=row, column=2).value is None:
+            break
+
+        excel_row['DepAff'] = worksheet.cell(row=row, column=2).value
+        excel_row['AdmArea'] = worksheet.cell(row=row, column=5).value
+        excel_row['District'] = worksheet.cell(row=row, column=6).value
+        excel_row['Location'] = worksheet.cell(row=row, column=7).value
+
+        excel_data.append(excel_row)
+        
+
+    return excel_data
+
+
+
+def show_excel(data):
+    print(data)
+
+
+if __name__ == '__main__':
+    excel_data = read_excel('sobaki.xlsx')
+    show_excel(excel_data)
