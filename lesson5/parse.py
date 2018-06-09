79feb6c4d928cfe557a8554a5acb3c0dd24d8844
Anastasiya Ovinnikova <anovinnikova@yandex.ru>
2018-06-09 14:09:22 +0300

    parsing practice

--- lesson5/parse.py
+++ lesson5/parse.py
@@ -5,19 +5,33 @@ from req import get_html
 html = get_html('https://yandex.ru/search/?lr=10742&text=python')
 
 
-bs = BeautifulSoup(html, 'html.parser')
+if html:
+    bs = BeautifulSoup(html, 'html.parser')
 
-#print(bs.prettify())
+    #print(bs.prettify())
 
 
-#print(bs.title)
-#print(bs.title.text) #bez tega
-#for par in bs.find_all('p'): #for loop po vsem p
-#    print(par.text.upper())
+    #print(bs.title)
+    #print(bs.title.text) #bez tega
+    #for par in bs.find_all('p'): #for loop po vsem p
+    #    print(par.text.upper())
 
-for item in bs.find_all('li', class_='serp-item'):
-    block_title = item.find('a', class_='organic__url')
-    print(block_title.text)
-    href = item.find('a', class_='path__item')
-    print(href)
-    
+
+    data = []
+
+
+    for item in bs.find_all('li', class_='serp-item'):
+        block_title = item.find('a', class_='organic__url')
+        href = item.find('a', class_='path__item')
+
+        data.append({
+            'title': block_title.text,
+            'link' : href.get('href')
+            })
+
+
+
+    print(data)
+
+else:
+    print('Что-то пошло не так..')
